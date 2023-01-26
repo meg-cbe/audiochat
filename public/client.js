8ddf8157e5676a545566a9c0d0fc840fba2c9121
prakashblaster <prakashbscit6396@gmail.com>
2023-01-26 15:09:53 +0530

    initial commit

---
+++ public/client.js
@@ -0,0 +1,242 @@
+//our username 
+var name;
+var connectedUser;
+
+//connecting to our signaling server 
+var conn = new WebSocket('ws://192.168.1.2:9090');
+
+conn.onopen = function () {
+   console.log("Connected to the signaling server");
+};
+
+//when we got a message from a signaling server 
+conn.onmessage = function (msg) {
+   console.log("Got message", msg.data);
+   var data = JSON.parse(msg.data);
+
+   switch (data.type) {
+      case "login":
+         handleLogin(data.success);
+         break;
+      //when somebody wants to call us 
+      case "offer":
+         handleOffer(data.offer, data.name);
+         break;
+      case "answer":
+         handleAnswer(data.answer);
+         break;
+      //when a remote peer sends an ice candidate to us 
+      case "candidate":
+         handleCandidate(data.candidate);
+         break;
+      case "leave":
+         handleLeave();
+         break;
+      default:
+         break;
+   }
+};
+
+conn.onerror = function (err) {
+   console.log("Got error", err);
+};
+
+//alias for sending JSON encoded messages 
+function send(message) {
+   //attach the other peer username to our messages 
+   if (connectedUser) {
+      message.name = connectedUser;
+   }
+
+   conn.send(JSON.stringify(message));
+};
+
+//****** 
+//UI selectors block 
+//****** 
+
+var loginPage = document.querySelector('#loginPage');
+var usernameInput = document.querySelector('#usernameInput');
+var loginBtn = document.querySelector('#loginBtn');
+
+var callPage = document.querySelector('#callPage');
+var callToUsernameInput = document.querySelector('#callToUsernameInput');
+var callBtn = document.querySelector('#callBtn');
+
+var hangUpBtn = document.querySelector('#hangUpBtn');
+var localAudio = document.querySelector('#localAudio');
+var remoteAudio = document.querySelector('#remoteAudio');
+
+var yourConn;
+var stream;
+
+callPage.style.display = "none";
+
+// Login when the user clicks the button 
+loginBtn.addEventListener("click", function (event) {
+   name = usernameInput.value;
+
+   if (name.length > 0) {
+      send({
+         type: "login",
+         name: name
+      });
+   }
+
+});
+
+function getUserMedia(constraints) {
+   // if Promise-based API is available, use it
+   if (navigator.mediaDevices) {
+      return navigator.mediaDevices.getUserMedia(constraints);
+   }
+
+   // otherwise try falling back to old, possibly prefixed API...
+   var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+   if (legacyApi) {
+      // ...and promisify it
+      return new Promise(function (resolve, reject) {
+         legacyApi.bind(navigator)(constraints, resolve, reject);
+      });
+   }
+}
+
+function getStream() {
+   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      alert('User Media API not supported.');
+      return;
+   }
+
+   var constraints = { video: false, audio: true };
+
+   getUserMedia(constraints).then(function (stream) {
+      if ('srcObject' in localAudio) {
+         localAudio.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+         localAudio.mozSrcObject = stream;
+      } else {
+         localAudio.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+
+      //using Google public stun server 
+      var configuration = {
+         "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+      };
+
+      yourConn = new webkitRTCPeerConnection(configuration);
+
+      // setup stream listening 
+      yourConn.addStream(stream);
+      //when a remote user adds stream to the peer connection, we display it 
+      yourConn.onaddstream = function (e) {
+         if ('srcObject' in localAudio) {
+            remoteAudio.srcObject = e.stream;
+         } else if (navigator.mozGetUserMedia) {
+            remoteAudio.mozSrcObject = e.stream;
+         } else {
+            remoteAudio.src = (window.URL || window.webkitURL).createObjectURL(e.stream);
+         }
+      };
+
+      // Setup ice handling 
+      yourConn.onicecandidate = function (event) {
+         if (event.candidate) {
+            send({
+               type: "candidate",
+               candidate: event.candidate
+            });
+         }
+      };
+   })
+      .catch(function (err) {
+         alert('Error: ' + err);
+      });
+}
+
+function handleLogin(success) {
+   if (success === false) {
+      alert("Ooops...try a different username");
+   } else {
+      loginPage.style.display = "none";
+      callPage.style.display = "block";
+
+      //********************** 
+      //Starting a peer connection 
+      //********************** 
+
+      //getting local audio stream 
+      getStream()
+
+   }
+};
+
+//initiating a call 
+callBtn.addEventListener("click", function () {
+   var callToUsername = callToUsernameInput.value;
+   if (callToUsername.length > 0) {
+      connectedUser = callToUsername;
+      // create an offer 
+      yourConn.createOffer(function (offer) {
+         console.log("offer")
+         send({
+            type: "offer",
+            offer: offer
+         });
+
+         yourConn.setLocalDescription(offer);
+      }, function (error) {
+         alert("Error when creating an offer");
+      });
+   }
+});
+
+//when somebody sends us an offer 
+function handleOffer(offer, name) {
+   connectedUser = name;
+   yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+
+   //create an answer to an offer 
+   yourConn.createAnswer(function (answer) {
+      yourConn.setLocalDescription(answer);
+
+      send({
+         type: "answer",
+         answer: answer
+      });
+
+   }, function (error) {
+      alert("Error when creating an answer");
+   });
+
+};
+
+//when we got an answer from a remote user 
+function handleAnswer(answer) {
+   yourConn.setRemoteDescription(new RTCSessionDescription(answer));
+};
+
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) {
+   yourConn.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+//hang up
+hangUpBtn.addEventListener("click", function () {
+   send({
+      type: "leave"
+   });
+
+   handleLeave();
+});
+
+function handleLeave() {
+   connectedUser = null;
+   remoteAudio.src = null;
+
+   yourConn.close();
+   yourConn.onicecandidate = null;
+   yourConn.onaddstream = null;
+};
---
+++ server.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+app.use((req, res, next) => {
+   res.append('Access-Control-Allow-Origin', ['*']);
+   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   res.append('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+});
+//require our websocket library 
+var WebSocketServer = require('ws').Server; 
+
+//creating a websocket server at port 9090 
+var wss = new WebSocketServer({port: 9090}); 
+
+//all connected to the server users 
+var users = {};
+
+//when a user connects to our sever 
+wss.on('connection', function(connection) {
+  
+   console.log("User connected");
+	
+   //when server gets a message from a connected user 
+   connection.on('message', function(message) { 
+	
+      var data;
+		
+      //accepting only JSON messages 
+      try { 
+         data = JSON.parse(message); 
+      } catch (e) { 
+         console.log("Invalid JSON"); 
+         data = {}; 
+      }
+		
+      //switching type of the user message 
+      switch (data.type) { 
+         //when a user tries to login 
+         case "login": 
+            console.log("User logged", data.name); 
+				
+            //if anyone is logged in with this username then refuse 
+            if(users[data.name]) { 
+               sendTo(connection, { 
+                  type: "login", 
+                  success: false 
+               }); 
+            } else { 
+               //save user connection on the server 
+               users[data.name] = connection; 
+               connection.name = data.name;
+					
+               sendTo(connection, { 
+                  type: "login", 
+                  success: true 
+               }); 
+            } 
+				
+            break;
+				
+         case "offer": 
+            //for ex. UserA wants to call UserB 
+            console.log("Sending offer to: ", data.name); 
+				
+            //if UserB exists then send him offer details 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               //setting that UserA connected with UserB 
+               connection.otherName = data.name; 
+					
+               sendTo(conn, { 
+                  type: "offer", 
+                  offer: data.offer, 
+                  name: connection.name 
+               }); 
+            } 
+				
+            break;
+				
+         case "answer": 
+            console.log("Sending answer to: ", data.name); 
+            //for ex. UserB answers UserA 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               connection.otherName = data.name; 
+               sendTo(conn, { 
+                  type: "answer", 
+                  answer: data.answer 
+               });
+            } 
+				
+            break;
+				
+         case "candidate": 
+            console.log("Sending candidate to:",data.name); 
+            var conn = users[data.name];  
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "candidate", 
+                  candidate: data.candidate 
+               }); 
+            } 
+				
+            break;
+				
+         case "leave": 
+            console.log("Disconnecting from", data.name); 
+            var conn = users[data.name]; 
+            conn.otherName = null; 
+				
+            //notify the other user so he can disconnect his peer connection 
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "leave" 
+               }); 
+            }  
+				
+            break;
+				
+         default: 
+            sendTo(connection, { 
+               type: "error", 
+               message: "Command not found: " + data.type 
+            });
+				
+            break; 
+      }  
+   });
+	
+   //when user exits, for example closes a browser window 
+   //this may help if we are still in "offer","answer" or "candidate" state 
+   connection.on("close", function() { 
+	
+      if(connection.name) { 
+         delete users[connection.name]; 
+			
+         if(connection.otherName) { 
+            console.log("Disconnecting from ", connection.otherName); 
+            var conn = users[connection.otherName]; 
+            conn.otherName = null;  
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "leave" 
+              }); 
+            }  
+         } 
+      } 
+   });  
+	
+   connection.send("Hello world"); 
+}); 
+ 
+function sendTo(connection, message) { 
+   connection.send(JSON.stringify(message)); 
+}
+app.use(express.static(__dirname + '/public'));
+router.get('/',function(req,res){
+   res.sendFile(path.join(__dirname+'/index.html'));
+   //__dirname : It will resolve to your project folder.
+ });
+
+ app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+console.log('http://localhost:3000');
